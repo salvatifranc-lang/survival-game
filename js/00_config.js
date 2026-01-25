a5a98513a2bd3508f8f87f1d95b199fee8491424
salvatifranc-lang <salvatifranc@gmail.com>
2026-01-25 23:28:36 +0100

    Update 00_config.js

--- js/00_config.js
+++ js/00_config.js
@@ -5,8 +5,6 @@
 const WORKER_ENDPOINT = "https://still-hat-5795.salvatifranc.workers.dev/";
 
 const WORKER_URL = "https://still-hat-5795.salvatifranc.workers.dev/";
-const MANUAL_URL =
-  "https://raw.githubusercontent.com/salvatifranc-lang/survival-game/main/HOPE_TOWN_GAME_MANUAL.txt";
 const START_MANUAL_URL = "HOPE_TOWN_START_MANUAL.txt";
 export const MANUAL_URL = "HOPE_TOWN_START_MANUAL.txt"; // o il path corretto
 
